198e47ec5940f3e45d2d75d66a45787a8bf896eb
Zayne Hussain <zayneh125@gmail.com>
2022-06-14 16:38:46 -0700

    started block class logic

--- Blood_Rush/src/Block.java
+++ Blood_Rush/src/Block.java
@@ -1,7 +1,30 @@
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.net.URL;
 
 public class Block {
 	private String type;
+	private Image img;
 	public Block (String t) {
 		type = t;
+		if (type.equals("grass")) {
+			img = getImage("grass.png");
+		}
+		else if (type.equals("snow")){
+			img = getImage("snow.png");
+		}
+		else {
+			img = getImage("desert.png");
+		}
+	}
+	private Image getImage(String path) {
+		Image tempImage = null;
+		try {
+			URL imageURL = Block.class.getResource(path);
+			tempImage = Toolkit.getDefaultToolkit().getImage(imageURL);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return tempImage;
 	}
 }
